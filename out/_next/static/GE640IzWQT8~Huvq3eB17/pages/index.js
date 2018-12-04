e810bcc1d06c7d656f8202aa19023b8faeccc8b4
Emilio Almansi <ealmansi@gmail.com>
2018-12-04 12:32:59 -0300

    Cleaned out

--- out/_next/static/GE640IzWQT8~Huvq3eB17/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{202:function(n,t,e){__NEXT_REGISTER_PAGE("/",function(){return n.exports=e(203),{page:n.exports.default}})},203:function(n,t,e){"use strict";e.r(t);var o=e(10),r=e.n(o);t.default=function(){return r.a.createElement("div",null,"Tortas Margo")}}},[[202,1,0]]]);
