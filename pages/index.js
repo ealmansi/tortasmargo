3e7fbb029fc98ab7781d22c2f6336d0ca49b6433
Emilio Almansi <ealmansi@gmail.com>
2018-12-04 12:23:36 -0300

    Added logo

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,19 @@
-export default () => <div>Tortas Margo</div>
+export default () => (
+  <div className='page'>
+    <div className='content'>
+      <img className='logo' src='/static/img/logo.png' alt='Tortas Margo'/>
+    </div>
+    <style jsx>{`
+      body {
+        position: relative;
+      }
+      .logo {
+        max-width: 90vw;
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    `}</style>
+  </div>
+)
