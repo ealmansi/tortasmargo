09f269497e7271fdadf3a56faca68e9cbb30d1e9
Emilio Almansi <ealmansi@gmail.com>
2018-12-04 12:43:25 -0300

    Added out.

---
+++ _next/static/webpack/static/development/pages/index.js.53df77d0a64fd1c3741e.hot-update.js
@@ -0,0 +1,72 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/home/ealmansi/dev/ealmansi/tortasmargo/pages/index.js";
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "jsx-2895629192" + " " + 'page',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "jsx-2895629192" + " " + 'content',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+    src: "/static/img/logo.png",
+    alt: "Tortas Margo",
+    className: "jsx-2895629192" + " " + 'logo',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    styleId: "2895629192",
+    css: "body.jsx-2895629192{position:relative;}.logo.jsx-2895629192{width:20vw;position:absolute;top:10vh;left:10vw;-webkit-transform:translate(-10vw,-10vh);-ms-transform:translate(-10vw,-10vh);transform:translate(-10vw,-10vh);}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL2VhbG1hbnNpL2Rldi9lYWxtYW5zaS90b3J0YXNtYXJnby9wYWdlcy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFLZ0IsQUFHMkIsQUFHUCxXQUNPLE9BSHBCLFdBSVcsU0FDQyxVQUN3QiwrR0FDcEMiLCJmaWxlIjoiL2hvbWUvZWFsbWFuc2kvZGV2L2VhbG1hbnNpL3RvcnRhc21hcmdvL3BhZ2VzL2luZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgKCkgPT4gKFxuICA8ZGl2IGNsYXNzTmFtZT0ncGFnZSc+XG4gICAgPGRpdiBjbGFzc05hbWU9J2NvbnRlbnQnPlxuICAgICAgPGltZyBjbGFzc05hbWU9J2xvZ28nIHNyYz0nL3N0YXRpYy9pbWcvbG9nby5wbmcnIGFsdD0nVG9ydGFzIE1hcmdvJy8+XG4gICAgPC9kaXY+XG4gICAgPHN0eWxlIGpzeD57YFxuICAgICAgYm9keSB7XG4gICAgICAgIHBvc2l0aW9uOiByZWxhdGl2ZTtcbiAgICAgIH1cbiAgICAgIC5sb2dvIHtcbiAgICAgICAgd2lkdGg6IDIwdnc7XG4gICAgICAgIHBvc2l0aW9uOiBhYnNvbHV0ZTtcbiAgICAgICAgdG9wOiAxMHZoO1xuICAgICAgICBsZWZ0OiAxMHZ3O1xuICAgICAgICB0cmFuc2Zvcm06IHRyYW5zbGF0ZSgtMTB2dywgLTEwdmgpO1xuICAgICAgfVxuICAgIGB9PC9zdHlsZT5cbiAgPC9kaXY+XG4pXG4iXX0= */\n/*@ sourceURL=/home/ealmansi/dev/ealmansi/tortasmargo/pages/index.js */",
+    __self: this
+  }));
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.53df77d0a64fd1c3741e.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.7ed966cc42b38d112000.hot-update.js
@@ -0,0 +1,72 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/home/ealmansi/dev/ealmansi/tortasmargo/pages/index.js";
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "jsx-3692417388" + " " + 'page',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "jsx-3692417388" + " " + 'content',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+    src: "/static/img/logo.png",
+    alt: "Tortas Margo",
+    className: "jsx-3692417388" + " " + 'logo',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    styleId: "3692417388",
+    css: "body.jsx-3692417388{position:relative;}.logo.jsx-3692417388{position:absolute;top:10vh;left:10vw;-webkit-transform:translate(-10vw,-10vh);-ms-transform:translate(-10vw,-10vh);transform:translate(-10vw,-10vh);}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL2VhbG1hbnNpL2Rldi9lYWxtYW5zaS90b3J0YXNtYXJnby9wYWdlcy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFLZ0IsQUFHMkIsQUFHQSxrQkFGcEIsQUFHVyxTQUNDLFVBQ3dCLCtHQUNwQyIsImZpbGUiOiIvaG9tZS9lYWxtYW5zaS9kZXYvZWFsbWFuc2kvdG9ydGFzbWFyZ28vcGFnZXMvaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gIDxkaXYgY2xhc3NOYW1lPSdwYWdlJz5cbiAgICA8ZGl2IGNsYXNzTmFtZT0nY29udGVudCc+XG4gICAgICA8aW1nIGNsYXNzTmFtZT0nbG9nbycgc3JjPScvc3RhdGljL2ltZy9sb2dvLnBuZycgYWx0PSdUb3J0YXMgTWFyZ28nLz5cbiAgICA8L2Rpdj5cbiAgICA8c3R5bGUganN4PntgXG4gICAgICBib2R5IHtcbiAgICAgICAgcG9zaXRpb246IHJlbGF0aXZlO1xuICAgICAgfVxuICAgICAgLmxvZ28ge1xuICAgICAgICBwb3NpdGlvbjogYWJzb2x1dGU7XG4gICAgICAgIHRvcDogMTB2aDtcbiAgICAgICAgbGVmdDogMTB2dztcbiAgICAgICAgdHJhbnNmb3JtOiB0cmFuc2xhdGUoLTEwdncsIC0xMHZoKTtcbiAgICAgIH1cbiAgICBgfTwvc3R5bGU+XG4gIDwvZGl2PlxuKVxuIl19 */\n/*@ sourceURL=/home/ealmansi/dev/ealmansi/tortasmargo/pages/index.js */",
+    __self: this
+  }));
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.7ed966cc42b38d112000.hot-update.js.map
