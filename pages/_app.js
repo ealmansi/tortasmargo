38fb75b6dfd4d6c26cf344750a67f76aca28097a
Emilio Almansi <ealmansi@gmail.com>
2018-12-05 15:41:55 -0300

    Added favicon and media preview.

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,11 @@ export default class MyApp extends App {
       <Container>
         <Head>
           <title>Tortas Margo&trade; | Tortas ricas y accesibles. A domicilio.</title>
+          <link rel='shortcut icon' type='image/png' href='/static/favicon.png'/>
+          <meta property='og:image' content='/static/images/og-image.png'></meta>
+          <meta property='og:image:type' content='image/png'></meta>
+          <meta property='og:image:width' content='500'></meta>
+          <meta property='og:image:height' content='500'></meta>
         </Head>
         <Component { ...pageProps } />
       </Container>
